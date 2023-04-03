79c460d4d4e277c31ed510e7c369c01ce03eacdf
eliashead <eliashead.costa@gmail.com>
2023-04-03 09:15:52 -0300

    feat: add class pokemom

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,10 +1,32 @@
 const pokeApi = {}
 
+function covertPokeApiDetailToPokemon(pokeDetail)  {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+    pokemon.types = types
+    pokemon.type = type
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+};
+
+pokeApi.getPokemonDeteial = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((reposnse) => reposnse.json())
+        .then(covertPokeApiDetailToPokemon)
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDeteial))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
         .catch((error) => console.log(error))
 }
